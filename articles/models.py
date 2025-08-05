cf2301c699561af3a8b03ae51827120ab71079d3
Sheng <custarder@hotmail.com>
2025-08-05 14:23:01 +0800

    feature: add articles

---
+++ articles/models.py
@@ -0,0 +1,6 @@
+from django.db import models
+
+# Create your models here.
+class Article(models.Model):
+    title = models.CharField(max_length=200, null=False)
+    content = models.TextField(null=True)
--- wretch/urls.py
+++ wretch/urls.py
@@ -1,8 +1,8 @@
 from django.urls import path, include
 from django.contrib import admin
 
-
 urlpatterns = [
     path("", include("pages.urls")),
+    path("articles", include("articles.urls")),
     path("admin/", admin.site.urls)
 ]
