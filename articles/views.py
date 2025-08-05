dccd2fe7e807f799555bef434f52b20e7585c355
Sheng <custarder@hotmail.com>
2025-08-05 15:52:00 +0800

    feature: create article

--- articles/views.py
+++ articles/views.py
@@ -1,5 +1,15 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .models import Article
 
 # Create your views here.
 def index(request):
-    return render(request, "pages/articles.html")
+    if request.POST:
+        title = request.POST["title"]
+        content = request.POST["content"]
+        Article.objects.create(title=title, content=content)
+        return redirect("pages:home")
+    else: 
+        return render(request, "pages/articles.html")
+
+def new(request):
+    return render(request, "pages/new.html")
